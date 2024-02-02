19288c3185481eedd566a337db20c4cb73582ddb
Hamza Khan <hamzasafdark@gmail.com>
2024-02-02 23:44:12 +0500

    update .gitignore

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,6 @@ const two: number = 33
 
 app.get('/', (req: any, res: { send: (arg0: string) => any }) => res.send(`1+2 = ${one + two}`))
 
-
 app.listen(port)
 
 console.log(`[app]: http://localhost:${port}`)
